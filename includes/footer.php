3913cf7d4e02543d9f020258ec6249ed5c7bbc11
Katherine Botabara <botakat3@gmail.com>
2026-02-02 00:58:34 -0600

    Fix nav and footer layout

--- includes/footer.php
+++ includes/footer.php
@@ -1,45 +1,71 @@
-<?php echo "
-<footer>
-    <div class='container'>
-        <div class='footer-content row text-center'>
-            <div class='col footer footer-left'>
-                <img id='footer-logo' src='images/logo_simple.webp' alt='Mama D's logo '>
-                <p>Receive a list of local happenings at Mama Ds and around the neighborhood.</p>
-                <form class='d-flex' role='search'>
-                    <input class='form-control me-2' type='search' placeholder='Email'>
-                    <button class='btn footer-btn' type='submit'>Submit!</button>
+<footer class="site-footer py-5">
+    <div class="container">
+        <div class="row text-center text-md-start align-items-start">
+
+            <!-- Left: Logo + Newsletter -->
+            <div class="col-md-4 mb-4 footer-left">
+                <img
+                    id="footer-logo"
+                    src="images/logo_simple.webp"
+                    alt="Mama D's Coffee logo"
+                    class="mb-3"
+                >
+
+                <p class="footer-text">
+                    Receive a list of local happenings at Mama D’s and around the neighborhood.
+                </p>
+
+                <form class="d-flex flex-column flex-sm-row gap-2" aria-label="Email signup">
+                    <input
+                        type="email"
+                        class="form-control"
+                        placeholder="Email address"
+                        aria-label="Email address"
+                        required
+                    >
+                    <button class="btn footer-btn" type="submit">Submit</button>
                 </form>
             </div>
-            <div class='col footer footer-middle'>
-                <div>
-                    <h4>Quick Links</h4>
-                    <ul>
-                        <li><a href='index.php'>Home</a></li>
-                        <li><a href='menu.php'>Menu</a></li>
-                        <li><a href='live.php'>Live Music</a></li>
-                        <li><a href='contact.php'>Contact</a></li>
-                        <li><a href='apply.php'>Apply</a></li>
-                        <li><a href='visit.php'>Visit Us</a></li>
 
-                    </ul>
-                </div>
-
-
-
+            <!-- Middle: Quick Links -->
+            <div class="col-md-4 mb-4 footer-middle">
+                <h4 class="footer-heading">Quick Links</h4>
+                <ul class="footer-links list-unstyled">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="menu.php">Menu</a></li>
+                    <li><a href="live.php">Live Music</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="apply.php">Apply</a></li>
+                    <li><a href="visit.php">Visit Us</a></li>
+                </ul>
             </div>
-            <div class='col footer footer-right'>
-                <h4>Our Socials</h4>
+
+            <!-- Right: Socials -->
+            <div class="col-md-4 footer-right text-center text-md-start">
+                <h4 class="footer-heading">Our Socials</h4>
                 <p>Connect with us!</p>
-                <div class='icon-container'>
-                <a href='https://www.facebook.com/MamaDsCoffee/' target='_blank'><img src='images/facebook-icon.webp' alt='Facebook icon'></a>
-                <a href='https://www.instagram.com/mamadscoffee/?hl=en' target='_blank'><img src='images/instagram-icon.webp' alt='Instagram icon'></a>
 
+                <div class="icon-container d-flex justify-content-center justify-content-md-start gap-3">
+                    <a
+                        href="https://www.facebook.com/MamaDsCoffee/"
+                        target="_blank"
+                        aria-label="Mama D's Facebook"
+                    >
+                        <img src="images/facebook-icon.webp" alt="Facebook icon">
+                    </a>
+
+                    <a
+                        href="https://www.instagram.com/mamadscoffee/?hl=en"
+                        target="_blank"
+                        aria-label="Mama D's Instagram"
+                            >
+                        <img src="images/instagram-icon.webp" alt="Instagram icon">
+                    </a>
                 </div>
             </div>
-        </div>
 
+        </div>
     </div>
 </footer>
 
-";
-?>
+
--- includes/navigation.php
+++ includes/navigation.php
@@ -1,34 +1,38 @@
-<?php echo "
+<?php
+echo "
 <header>
-    <nav class='navbar navbar-expand-lg '>
-        <div class='container-fluid'>
-            <a class='navbar-brand logo' href='index.php'><img src='images/logo_simple.webp' alt='Mama D's logo'></a>
-            <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-                <span class='navbar-toggler-icon'></span>
-            </button>
-            <div class='collapse navbar-collapse' id='navbarNav'>
-                <ul class='navbar-nav'>
-                    <li class='nav-item'>
-                        <a class='nav-link' href='menu.php '>Menu</a>
-                    </li>
-                    <li class='nav-item'>
-                        <a class='nav-link' href='live.php'>Live Music</a>
-                    </li>
-                    <li class='nav-item'>
-                        <a class='nav-link' href='contact.php'>Contact</a>
-                    </li>
-                    <li class='nav-item'>
-                        <a class='nav-link' href='apply.php'>Apply</a>
-                    </li>
-                    <li class='nav-item'>
-                        <button type='button' id='visit' class='btn'><a class='nav-link' href='visit.php'>Visit Us</a></button>
-                    </li>
+  <nav class='navbar navbar-expand-lg'>
+    <div class='container-fluid'>
+      <a class='navbar-brand logo' href='index.php' aria-label='Mama D\\'s Home'>
+        <img src='images/logo_simple.webp' alt='Mama D\\'s logo' loading='lazy'>
+      </a>
 
-                </ul>
-            </div>
-        </div>
-    </nav>
+      <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav'
+        aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+        <span class='navbar-toggler-icon'></span>
+      </button>
 
+      <div class='collapse navbar-collapse' id='navbarNav'>
+        <ul class='navbar-nav ms-auto align-items-lg-center gap-lg-2'>
+          <li class='nav-item'>
+            <a class='nav-link' href='menu.php'>Menu</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='live.php'>Live Music</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='contact.php'>Contact</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='apply.php'>Apply</a>
+          </li>
+          <li class='nav-item'>
+            <a class='btn btn-primary nav-cta' href='visit.php'>Visit Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </header>
 ";
 ?>
